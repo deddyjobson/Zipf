ced0b948bd2e447486d86a36d328fcd4181692c2
deddyjobson <dedjob@hotmail.com>
2017-05-04 17:25:51 +0530

    Frequency of occurences plotted

--- code.py
+++ code.py
@@ -1,11 +1,31 @@
-from numpy import *
+from __future__ import division
+from pylab import *
 try:
     import Image
 except ImportError:
     from PIL import Image
 from pytesseract import image_to_string
+import string
 
 img = Image.open('eng.jpg')
 story = image_to_string(img, lang = 'eng')
 
-print story
+#To do away with punctuations
+replace_punc = string.maketrans(string.punctuation,
+    ' '*len(string.punctuation))
+story = story.translate(replace_punc)
+
+words = {}
+for w in filter(None,story.split(' ')):
+    if w in words:
+        words[w]+=1
+    else:
+        words[w] = 1
+
+y = array(sorted(words.values(), reverse = True))
+
+x = arange(1,len(y)+1)
+plot(x,y)
+title('Number of occurences of words')
+y0 = 1/y[y>1]
+show()
